04985f5adf2419639556e5d21ef00dec40c7e671
Rumana Khanam <aakhi.zaman@gmail.com>
2025-04-05 16:01:36 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +8,5 @@ def home():
     return "Hello from Khanam ECS Container."
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
