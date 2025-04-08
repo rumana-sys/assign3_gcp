872c137819c2d4673a4a3d8065e8431d5f836de4
Rumana Khanam <aakhi.zaman@gmail.com>
2025-04-08 19:08:42 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,5 +8,5 @@ def home():
     return "Hello from Khanam ECS Container."
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
